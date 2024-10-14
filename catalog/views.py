4f200018b41876a66c02435b8354b40d39d93fd3
DeMarkiz <markberezhnoy27@gmail.com>
2024-10-14 17:20:00 +0300

    ff

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        return HttpResponse(f"Дай старый Бог вам здоровья {name}")
+    return render(request, 'contact.html')
